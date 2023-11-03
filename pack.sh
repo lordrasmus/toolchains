6399d9c11424d560ef59dec768d007d7d53fc76c
ramin <lordrasmus@gmail.com>
2023-11-03 10:54:44 +0100

    pack.sh update

--- pack.sh
+++ pack.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-tc=$1
-
-if [ $# -eq 0 ] ; then
-	echo "keine Toolchain angegeben"
-	exit 1
-fi
-
-echo "Toolchain : $1"
-
-if [[ $tc == */ ]]; then
-	echo "  ohne / am ende angeben"
-	exit 1
-fi
-
-if [ ! -e $tc ] ; then
-	echo "  nicht gefunden"
-	exit 1
-fi
 
 arch=$(grep ADK_TARGET_CPU_ARCH= .config)
 arch="${arch/ADK_TARGET_CPU_ARCH=}"
@@ -40,8 +22,6 @@ echo "GCC : $gcc"
 echo "ARCH: $arch"
 echo ""
 
-cp .config $tc/config
-cp /etc/os-release $tc
 
 
 target=$(grep ADK_TARGET_SYSTEM= .config)
@@ -54,27 +34,33 @@ lib="${lib/ADK_TARGET_LIBC=}"
 lib="${lib/\"}"
 lib="${lib/\"}"
 
+
+
 tc2="toolchain-"$arch"-gcc-"$gcc
 
+build_path="toolchain_"$target"_"$lib
 sysroot_path="target_"$target"_"$lib
 
 if grep -q 'ADK_TARGET_CPU_TYPE' .config; then
-	tmp=$(grep ADK_TARGET_CPU_TYPE= .config)
-	tmp="${tmp/ADK_TARGET_CPU_TYPE=}"
-	tmp="${tmp/\"}"
-	tmp="${tmp/\"}"
+        tmp=$(grep ADK_TARGET_CPU_TYPE= .config)
+        tmp="${tmp/ADK_TARGET_CPU_TYPE=}"
+        tmp="${tmp/\"}"
+        tmp="${tmp/\"}"
 
-	sysroot_path=$sysroot_path"_"$tmp
-	tc2="toolchain-"$arch"_"$tmp"-gcc-"$gcc
+        build_path=$build_path"_"$tmp
+        sysroot_path=$sysroot_path"_"$tmp
+        tc2="toolchain-"$arch"_"$tmp"-gcc-"$gcc
 fi
 
 if grep -q 'ADK_TARGET_FLOAT' .config; then
-	tmp=$(grep ADK_TARGET_FLOAT= .config)
-	tmp="${tmp/ADK_TARGET_FLOAT=}"
-	tmp="${tmp/\"}"
-	tmp="${tmp/\"}"
-	sysroot_path=$sysroot_path"_"$tmp
-	tc2=$tc2"_"$tmp
+        tmp=$(grep ADK_TARGET_FLOAT= .config)
+        tmp="${tmp/ADK_TARGET_FLOAT=}"
+        tmp="${tmp/\"}"
+        tmp="${tmp/\"}"
+
+        build_path=$build_path"_"$tmp
+        sysroot_path=$sysroot_path"_"$tmp
+        tc2=$tc2"_"$tmp
 fi
 
 if grep -q 'ADK_TARGET_ABI' .config; then
@@ -82,6 +68,8 @@ if grep -q 'ADK_TARGET_ABI' .config; then
         tmp="${tmp/ADK_TARGET_ABI=}"
         tmp="${tmp/\"}"
         tmp="${tmp/\"}"
+
+        build_path=$build_path"_"$tmp
         sysroot_path=$sysroot_path"_"$tmp
         tc2=$tc2"_"$tmp
 fi
@@ -91,22 +79,70 @@ if grep -q 'ADK_TARGET_INSTRUCTION_SET' .config; then
         tmp="${tmp/ADK_TARGET_INSTRUCTION_SET=}"
         tmp="${tmp/\"}"
         tmp="${tmp/\"}"
+
+        build_path=$build_path"_"$tmp
         sysroot_path=$sysroot_path"_"$tmp
         tc2=$tc2"_"$tmp
 fi
 
+if grep -q 'BUSYBOX_NOMMU=y' .config; then
+        tmp=$(grep BUSYBOX_NOMMU=y .config)
+        tmp="${tmp/BUSYBOX_NOMMU=y}"
+        tmp="${tmp/\"}"
+        tmp="${tmp/\"}"
+        
+        build_path=$build_path"_nommu"
+        sysroot_path=$sysroot_path"_nommu"
+        tc2=$tc2"_nommu"
+fi
+
+
+if [ ! -e $build_path/usr/bin ] ; then
+        echo "Toolchain unvollständig : suche $build_path/usr/bin"
+        exit 1
+fi
 
 if [ ! -e $sysroot_path/usr/lib/crt1.o ] ; then
-	echo "Sysroot unvollständig : suche $sysroot_path"
-	exit 1
+        echo "Sysroot unvollständig   : suche $sysroot_path"
+        exit 1
 fi
+
+
+echo "Toolchain : $build_path"
+echo "Sysroot   : $sysroot_path"
+echo -e "Archive   : \033[01;32m$tc2\033[00m"
+
+
+tc=$build_path
+
+
+if [ -e $tc2.tar.xz ] ; then
+
+        echo ""
+        echo "Archive exists. Do you want to proceed? (y/n): "
+        read response
+
+        # Check if the input is "y" or "n"
+        if [ "$response" = "y" ]; then
+            echo "You entered 'y'. Proceeding."
+        elif [ "$response" = "n" ]; then
+            echo "You entered 'n'. Aborting."
+            exit 1
+        else
+            echo "Invalid input. Please enter 'y' or 'n'."
+            exit 1
+        fi
+fi
+
+
+git rev-parse HEAD > $tc/openadk_hash
+cp .config $tc/config
+cp /etc/os-release $tc
+
 
 mkdir -p $tc/sysroot
 cp -r $sysroot_path/* $tc/sysroot
 
-
-#echo "$tc2"
-#exit 1
 
 
 rm -rf $tc2
@@ -126,4 +162,22 @@ if [ ! -e toolchains ] ; then
 	exit 1
 fi
 cp $tc2.tar.xz toolchains
+
+
+
+echo ""
+echo -e "\033[01;32mstaring git commit. Do you want to proceed? (y/n):\033[00m "
+echo ""
+read response
+
+# Check if the input is "y" or "n"
+if [ "$response" = "y" ]; then
+    echo "You entered 'y'. Proceeding."
+elif [ "$response" = "n" ]; then
+    echo "You entered 'n'. Aborting."
+    exit 1
+else
+    echo "Invalid input. Please enter 'y' or 'n'."
+    exit 1
+fi
 ( cd toolchains; git add $tc2.tar.xz; git commit -m "toolchain $tc2" )
