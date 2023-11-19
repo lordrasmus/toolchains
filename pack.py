a99e3f4244fe4b02d38ed86922a17613876c6b09
ramin <lordrasmus@gmail.com>
2023-11-19 11:38:58 +0100

    toolchain toolchain-nds32le_nds32-gcc-13.2.0

--- pack.py
+++ pack.py
@@ -230,8 +230,16 @@ os.system("mkdir -p " +tc+"/sysroot")
 os.system("cp -r " + sysroot_path +"/* "+ tc +"/sysroot")
 
 
+
+
 os.system("rm -rf " + tc2 )
 os.system("cp -r " + tc + " " + tc2 )
+
+os.system("cd "+ tc2 + "/usr/" + prefix[:-1] + " ; rm lib; ln -s ../../sysroot/usr/lib lib"  )
+os.system("cd "+ tc2 + "/usr/" + prefix[:-1] + " ; rm sys-include; ln -s ../../sysroot/usr/include sys-include"  )
+#os.system("ls "+ tc2 + "/usr/" + prefix[:-1] + "/lib/ -all "  )
+
+#exit(1)
 
 os.system("rm -f " + tc2 + ".tar.xz")
 os.system("tar -cf " + tc2 + ".tar " +tc2 )
